9e05432190397586c31dc64cf4bb628cc9f16ecc
Hirokuni45 <h-takeyama@gatariweb.com>
2017-08-16 00:39:35 +0900

    rss のデータを保存する過程で疑問が

--- app/Console/Command/RssShell.php
+++ app/Console/Command/RssShell.php
@@ -4,21 +4,24 @@
 
   class RssShell extends AppShell{
 
+    public $uses = ['Post'];
+
     const RSS_URL = 'https://headlines.yahoo.co.jp/rss/abema-c_int.xml';
 
     public function main(){
 
       $results = Feed::load('https://headlines.yahoo.co.jp/rss/abema-c_int.xml');
-      $tmpArray = json_decode(json_encode($results), true);
-      ob_start();
-      new dBug($tmpArray);
-      $out = ob_get_contents();
-      ob_end_clean();
-      $path = WWW_ROOT . 'shell.html';
-      file_put_contents($path, $out);
-      exit;
+      #$tmpArray = json_decode(json_encode($results), true);
+      #ob_start();
+      #new dBug($tmpArray);
+      #$out = ob_get_contents();
+      #ob_end_clean();
+      #$path = WWW_ROOT . 'shell.html';
+      #file_put_contents($path, $out);
+      #exit;
       foreach($results["channel"]['item'] as $item){
-        var_dump($item['title']);
+        #$this->Post->save($item);
+        var_dump($item);
       }
 
 
